1c561cba0d22dc926b7a30ae793ca3a67ae40331
Aankirz <sahuankit453@gmail.com>
2023-03-01 13:45:15 +0530

    components folder added

---
+++ sac-website/components/Announcement/Announcement.js
@@ -0,0 +1,6 @@
+
+export default function Announcement() {
+  return (
+    <div>Announcement</div>
+  )
+}
---
+++ sac-website/components/FestCaraousel/FestCaraousel.js
@@ -0,0 +1,7 @@
+
+
+export default function FestCaraousel() {
+  return (
+    <div>FestCaraousel</div>
+  )
+}
