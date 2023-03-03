0067ce147f40f6b200d2692377388eb5d59ec0c2
Aashirbad Sabat <aum.aashirbad@gmail.com>
2023-03-03 17:51:21 +0530

    Announcement Page done

--- sac-website/components/Announcement/Announcement.js
+++ sac-website/components/Announcement/Announcement.js
@@ -1,6 +1,131 @@
-
 export default function Announcement() {
   return (
-    <div>Announcement</div>
-  )
+    <div className="lg:flex justify-center">
+      <div className="lg:w-[50%] w-full mt-4 pt-4">
+        <div className="text-3xl ml-3 mb-6 font-bold">Upcoming Events</div>
+        <div className="container mx-auto">
+          <div className="grid-cols-3 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3 mx-3 lg:mx-0">
+            <div className="w-full col-span-2 row-span-2 rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
+              <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80" />
+            </div>
+            <div className="w-full rounded ransition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
+              <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80" />
+            </div>
+            <div className="w-full rounded ransition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
+              <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80" />
+            </div>
+            <div className="w-full rounded ransition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
+              <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80" />
+            </div>
+            <div className="w-full rounded ransition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
+              <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80" />
+            </div>
+            <div className="w-full rounded ransition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
+              <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div className="py-4 mt-4">
+            <h1 className="text-3xl text-center font-bold mb-6">
+              Announcement
+            </h1>
+            <div className="container">
+              <div className="flex flex-col lg:grid grid-cols-12 text-gray-50 mr-1">
+                <div className="flex lg:contents">
+                  <div className="col-start-2 col-end-4 mr-2 lg:mx-auto relative">
+                    <div className="h-full w-6 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-100 pointer-events-none"></div>
+                    </div>
+                  </div>
+                  <div className="bg-[#e5383b] col-start-4 col-end-12 p-4 rounded-xl my-2 mr-auto shadow-md lg:w-96 w-full">
+                    <div className="font-semibold text-lg mb-1">
+                      Lorem Ipsum
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex lg:contents">
+                  <div className="col-start-2 col-end-4 mr-2 lg:mx-auto relative">
+                    <div className="h-full w-6 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-100 pointer-events-none"></div>
+                    </div>
+                  </div>
+                  <div className="bg-[#e5383b] col-start-4 col-end-12 p-4 rounded-xl my-2 mr-auto shadow-md lg:w-96 w-full">
+                    <div className="font-semibold text-lg mb-1">
+                      Lorem Ipsum
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex lg:contents">
+                  <div className="col-start-2 col-end-4 mr-2 lg:mx-auto relative">
+                    <div className="h-full w-6 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-100 pointer-events-none"></div>
+                    </div>
+                  </div>
+                  <div className="bg-[#e5383b] col-start-4 col-end-12 p-4 rounded-xl my-2 mr-auto shadow-md lg:w-96 w-full">
+                    <div className="font-semibold text-lg mb-1 text-gray-50">
+                      Lorem Ipsum
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex lg:contents">
+                  <div className="col-start-2 col-end-4 mr-2 lg:mx-auto relative">
+                    <div className="h-full w-6 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-100 pointer-events-none"></div>
+                    </div>
+                  </div>
+                  <div className="bg-[#e5383b] col-start-4 col-end-12 p-4 rounded-xl my-2 mr-auto shadow-md lg:w-96 w-full">
+                    <div className="font-semibold text-lg mb-1 text-white">
+                      Lorem Ipsum
+                    </div>
+                  </div>
+                </div>
+                <div className="flex lg:contents">
+                  <div className="col-start-2 col-end-4 mr-2 lg:mx-auto relative">
+                    <div className="h-full w-6 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-100 pointer-events-none"></div>
+                    </div>
+                  </div>
+                  <div className="bg-[#e5383b] col-start-4 col-end-12 p-4 rounded-xl my-2 mr-auto shadow-md lg:w-96 w-full">
+                    <div className="font-semibold text-lg mb-1 text-white">
+                      Lorem Ipsum
+                    </div>
+                  </div>
+                </div>
+                <div className="flex lg:contents">
+                  <div className="col-start-2 col-end-4 mr-2 lg:mx-auto relative">
+                    <div className="h-full w-6 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-100 pointer-events-none"></div>
+                    </div>
+                  </div>
+                  <div className="bg-[#e5383b] col-start-4 col-end-12 p-4 rounded-xl my-2 mr-auto shadow-md lg:w-96 w-full">
+                    <div className="font-semibold text-lg mb-1 text-white">
+                      Lorem Ipsum
+                    </div>
+                  </div>
+                </div>
+                <div className="flex lg:contents">
+                  <div className="col-start-2 col-end-4 mr-2 lg:mx-auto relative">
+                    <div className="h-full w-6 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-100 pointer-events-none"></div>
+                    </div>
+                  </div>
+                  <div className="bg-[#e5383b] col-start-4 col-end-12 p-4 rounded-xl my-2 mr-auto shadow-md lg:w-96 w-full">
+                    <div className="font-semibold text-lg mb-1 text-white">
+                      Lorem Ipsum
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
