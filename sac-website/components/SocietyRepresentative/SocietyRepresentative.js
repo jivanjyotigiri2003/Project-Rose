7d5963ea2a834ed35675cbd55a7a80c0d6a37a87
Anurag Narayan Jena <anj20@Anurags-MacBook-Air.local>
2023-03-10 16:16:02 +0530

    Updated The Representative

--- sac-website/components/SocietyRepresentative/SocietyRepresentative.js
+++ sac-website/components/SocietyRepresentative/SocietyRepresentative.js
@@ -2,67 +2,68 @@ import React from "react";
 
 const SocietyRepresentative = () => {
   return (
-    <div className="flex h-[100vh] flex-col bg-[rgba(255,246,195,0.33)]">
-      <div className="h-[20%] text-center font-[ptsans] text-[4rem] ">
+    <div class="flex xl:h-[100vh] xl:w-[100vw] h-[400vh] w-[100vw] flex-col bg-[rgba(255,246,195,0.33)]">
+      <div class="h-[10%] p-9 pt-14 font-[ptsans] text-3xl sm:text-6xl md:text-7xl ">
         Representative
       </div>
-      <div className="flex h-[60%] flex-col md:flex-row my-10 ml-20">
-        <div className="w-[30%] mx-2 flex flex-col">
+
+      <div class="flex h-[80%] flex-col lg:flex-row my-10 ml-20 p-4 gap-9">
+        <div class="lg:w-[20%] w-[80%] mx-2 flex flex-col">
           <div>
             <img
               src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
               alt=""
-              className="w-[100%] h-[45vh]  rounded-full shadow"
+              class="xl:w-96 xl:h-96 w-[70vw] aspect-square bg-cover rounded-full shadow"
             />
           </div>
           <br />
-          <div>
-            <p className="font-bold w-[100%]">Lorem</p>
+          <div class="text-center">
+            <p class="font-bold text-xl sm:text-3xl md:text-5xl">Lorem</p>
             <p>Lorem, ipsum.</p>
           </div>
         </div>
 
-        <div className="w-[30%] mx-2 flex flex-col">
+        <div class="lg:w-[20%] w-[80%] mx-2 flex flex-col">
           <div>
             <img
               src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
               alt=""
-              className="w-[100%] h-[45vh] rounded-full shadow"
+              class="xl:w-96 xl:h-96 w-[80vw] aspect-square bg-cover rounded-full shadow"
             />
           </div>
           <br />
-          <div>
-            <p className="font-bold">Lorem</p>
+          <div class="text-center">
+            <p class="font-bold text-xl sm:text-3xl md:text-5xl">Lorem</p>
             <p>Lorem, ipsum.</p>
           </div>
         </div>
 
-        <div className="w-[30%] mx-2 flex flex-col">
+        <div class="lg:w-[20%] w-[80%] mx-2 flex flex-col">
           <div>
             <img
               src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
               alt=""
-              className="w-[100%] h-[45vh] rounded-full shadow"
+              class="xl:w-96 xl:h-96 w-[80vw] aspect-square bg-cover rounded-full shadow"
             />
           </div>
           <br />
-          <div>
-            <p className="font-bold">Lorem</p>
+          <div class="text-center">
+            <p class="font-bold text-xl sm:text-3xl md:text-5xl">Lorem</p>
             <p>Lorem, ipsum.</p>
           </div>
         </div>
 
-        <div className="w-[30%] mx-2 flex flex-col">
+        <div class="lg:w-[20%] w-[80%] mx-2 flex flex-col">
           <div>
             <img
               src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
               alt=""
-              className="w-[100%] h-[45vh]  rounded-full shadow"
+              class="xl:w-96 xl:h-96 w-[80vw] aspect-square bg-cover rounded-full shadow"
             />
           </div>
           <br />
-          <div>
-            <p className="font-bold">Lorem</p>
+          <div class="text-center">
+            <p class="font-bold text-xl sm:text-3xl md:text-5xl">Lorem</p>
             <p>Lorem, ipsum.</p>
           </div>
         </div>
