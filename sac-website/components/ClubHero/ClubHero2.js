e304b0751a3badfb60f8f8294137fc40dcc6468a
Anubhab <anubhabpatra02@gmail.com>
2023-03-13 22:29:55 +0530

    footer design added

--- sac-website/components/ClubHero/ClubHero2.js
+++ sac-website/components/ClubHero/ClubHero2.js
@@ -3,11 +3,13 @@ import React from 'react'
 const ClubHero2 = () => {
   return (
             
-    <div className='w-full h-[100vh] '>
+    <div className='w-full h-[100vh]  '>
 
     <div className="w-full md:h-full mt-0 absolute   
 		bg-no-repeat bg-cover bg-center
 		bg-[url('https://res.cloudinary.com/dnqipwdsl/image/upload/v1678682263/webwiz_zgemsm.jpg')]">
+      
+     
     
 <div class="w-full h-[33vh] max-w-sm  rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto md:my-[30vh] my-[34vh]  ">
    
@@ -16,9 +18,9 @@ const ClubHero2 = () => {
     <div class="flex flex-col items-center ">
         <img class="lg:w-52 lg:h-52 md:w-64 md:h-64 w-32 h-32 lg:mb-3 md:mb-5 mb-6 rounded-full shadow-lg  border-4" src="https://res.cloudinary.com/dzbax077h/image/upload/v1677689847/WEBWIZ_BLACK_BACKGROUD_LOGO_1_cjuiib.png" alt="Bonnie image"/>
         <h5 class="lg:my-2 md:my-4 my- md:text-4xl lg:text-3xl font-bold tracking-widest text-white ">WEBWIZ</h5>
-        <span class="lg:text-sm xl:text-xl md:text-xl text-gray-300 text-center md:w-[50vw] lg:w-[36vw] w-[80vw] my-2 ">The Official Web Development Society of National Institute of Technology, Rourkela </span>
+        <span class="lg:text-sm xl:text-xl md:text-xl text-gray-300 text-center md:w-[50vw] lg:w-[36vw] w-[80vw] my-2 ">The Official Web Development Society of National Institute of Technology, Rourkela </span> </div>
     </div>
-    </div>
+    
 </div>
 
    
