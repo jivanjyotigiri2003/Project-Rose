82baba8ad624861722a5f01762636fca2f388a79
Aashirbad Sabat <aum.aashirbad@gmail.com>
2023-03-13 21:09:22 +0530

    Marquee changes made

--- sac-website/components/SocietyRepresentative/SocietyRepresentative.jsx
+++ sac-website/components/SocietyRepresentative/SocietyRepresentative.jsx
@@ -55,14 +55,12 @@ export default function SocietyRepresentative() {
   ];
 
   return (
-    <div className="w-screen">
+    <div className="w-screen ">
       <Fade direction="up" triggerOnce="true" delay="50">
-        <h1 className="w-[100%] h-[7%] text-[2vw] font-[600] p-4">
-          Sac Officials
-        </h1>
+        <h1 className="w-[100%] text-xl font-[600] p-4">Sac Officials</h1>
 
         {/* For Desktop */}
-        <div className="max-[500px]:hidden w-[100%] h-[90%] flex justify-center items-center p-[2rem] flex-wrap border-2  ">
+        <div className="w-[100%] h-[90%] flex justify-center items-center p-[2rem] flex-wrap border-2  ">
           {representative.map((item, index) => {
             return (
               <div
@@ -77,37 +75,11 @@ export default function SocietyRepresentative() {
                   />
                 </div>
 
-                <h2 className="font-bold text-[1.1vw]">{item.name}</h2>
-                <h5 className="text-[.9vw]">{item.designation}</h5>
+                <h2 className="font-bold text-lg">{item.name}</h2>
+                <h5 className="text-lg">{item.designation}</h5>
               </div>
             );
           })}
-        </div>
-
-        {/* For Mobile */}
-
-        <div className="min-[500px]:hidden h-[95%] flex justify-center items-center p-[2rem] flex-wrap border-2 border-solid border-indigo-500">
-          <SocietyCarousel>
-            {representative.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className="h-[20rem]  flex flex-col justify-center items-center m-[2rem] border-2 border-solid border-indigo-500"
-                >
-                  <div className="w-[15rem] h-[15rem]   rounded-[100%]  mb-[1rem] drop-shadow-xl">
-                    <img
-                      src={item.officialsImage}
-                      alt="officialsImage"
-                      className="w-[100%] h-[100%] rounded-[100%] object-cover"
-                    />
-                  </div>
-
-                  <h2 className="font-bold text-[2.7vh]">{item.name}</h2>
-                  <h5 className="text-[2vh]">{item.designation}</h5>
-                </div>
-              );
-            })}
-          </SocietyCarousel>
         </div>
       </Fade>
     </div>
--- sac-website/pages/_app.js
+++ sac-website/pages/_app.js
@@ -1,30 +1,24 @@
-import { Poppins } from 'next/font/google';
-
+import { Poppins } from "next/font/google";
 
 import Navbar from "@/components/Navbar/Navbar";
 import "@/styles/globals.css";
 import "../styles/societyGallery.css";
 
-import Footer from "@/components/Footer/Footer"
+import Footer from "@/components/Footer/Footer";
 
-import { Roboto } from 'next/font/google'
+import { Roboto } from "next/font/google";
 
 const poppins = Poppins({
-  weight: ['100','200','300','400','500','600','700','800','900'],
-  subsets: ['latin'],
- 
-})
-
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
-
-   
     <main className={poppins.className}>
       <div className=" absolute top-0 z-40 w-full bg-transparent">
-
-        <Navbar />
-      </div> */}
+        {/* <Navbar /> */}
+      </div>
       <Component {...pageProps} />
       <Footer />
       {/* <Callandar /> */}
