45f9b2f470231211bf7471cf5a0ca7664d977396
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2023-03-13 17:13:58 +0530

    ft:responsive

--- sac-website/components/ContactPage/ContactPage.jsx
+++ sac-website/components/ContactPage/ContactPage.jsx
@@ -4,10 +4,13 @@ import { BsFillEnvelopeFill } from "react-icons/bs";
 import { BiChevronDown } from "react-icons/bi";
 import faq from "../../public/faq.webp";
 import ProfileCard from "../ProfileCard/ProfileCard";
+import dean from "../../public/dean.jpg";
+import { Fade } from "react-awesome-reveal";
+import Image from "next/image";
 
 function ContactPage() {
   return (
-    <div className="relative min-h-screen">
+    <div className="">
       {/* Sac management page */}
       <div className="bg-[url('https://res.cloudinary.com/dkqxnquga/image/upload/v1678430362/image_767_hnhpi2.png')] bg-no-repeat bg-cover bg-center w-full min-h-screen overflow-x-hidden">
         <div className="  bg-gradient-to-b from-[#850101cc] to-[#8501011a] bg-no-repeat w-full bg-cover bg-center min-h-screen">
@@ -21,35 +24,171 @@ function ContactPage() {
         </div>
       </div>
 
-      {/* Contact details - Main*/}
+      <div className="w-screen min-h-screen bg-[#FFFFFF] -z-10">
+        {/* Contact details - Main*/}
 
-      <div className="w-full min-h-screen relative">
-        <div className="bg-[#A4161A] bg-no-repeat w-screen  bg-cover bg-center flex space-x-2 md:space-x-20 h-72 lg:h-[55vh]  lg:pt-[26vh]  px-[2vw] md:px-[20vw]">
-          <ProfileCard designation={"President SAC"} />
-          <ProfileCard designation={"Dean SW"} />
-        </div>
-        <div className="bg-[#FFFFFF] bg-no-repeat w-screen bg-cover bg-center h-[45vh]"></div>
-      </div>
-
-      {/* Lcs representatives */}
-      <div className="w-full min-h-screen  relative">
-        <div className="bg-[#A4161A] bg-no-repeat w-screen  bg-cover bg-center flex flex-col lg:h-[140rem] pt-[15vh]   ">
-          <h1 className="text-white font-semibold text-center text-xl md:text-6xl pb-[10vh]">
-            LCS Representatives
-          </h1>
-
-          <div className="grid grid-cols-2 lg:grid-cols-3 gap-x-[1.5rem] gap-y-[20rem] lg:gap-x-[4rem] lg:gap-y-[40rem] px-[1vw] lg:px-[5vw]">
-            <ProfileCard designation={"Vice President"} />
-            <ProfileCard designation={"Vice President"} />
-            <ProfileCard designation={"Secretary"} />
-            <ProfileCard designation={"Secretary"} />
-            <ProfileCard designation={"Secretary"} />
-            <ProfileCard designation={"Secretary"} />
+        <div className=" relative">
+          <div className="bg-[#A4161A]  absolute top-0 right-0 left-0 h-[50vh] w-full">
+            <div className=" mt-56 lg:mt-72 grid grid-cols-2 lg:mx-[22rem] mx-4 relative">
+              <ProfileCard designation={"President SAC"} />
+              <ProfileCard designation={"Dean SW"} />
+            </div>
           </div>
         </div>
       </div>
 
-     
+      {/* Lcs representatives */}
+      <div className="w-screen h-[1170px] lg:h-[2080px] bg-[#FFFFFF] -z-10">
+        <div className=" relative">
+          <div className="bg-[#A4161A] absolute top-0 right-0 left-0 h-[50vh] w-full   ">
+            <h1 className="text-white font-semibold text-center text-xl mt-20 lg:mt-25 lg:text-6xl ">
+              LCS Representatives
+            </h1>
+
+            <div className="grid grid-cols-2 lg:grid-cols-3 mx-1 lg:mx-28 mt-24 lg:mt-40 gap-10  relative">
+              <ProfileCard designation={"Vice President"} />
+              <ProfileCard designation={"Vice President"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+            </div>
+
+            <div className="w-full flex mt-7 lg:mt-14 justify-center items-center">
+              <button className="flex flex-col items-center justify-center ">
+                <h1 className={" text-[#A4161A]  font-semibold  text-xl"}>
+                  See More
+                </h1>
+                <BiChevronDown className="text-[#A4161A] text-2xl" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Fms representatives */}
+      <div className="w-screen h-[1170px] lg:h-[2080px] bg-[#FFFFFF] -z-10">
+        <div className=" relative">
+          <div className="bg-[#A4161A] absolute top-0 right-0 left-0 h-[50vh] w-full   ">
+            <h1 className="text-white font-semibold text-center text-xl mt-20 lg:mt-25 lg:text-6xl ">
+              FMS Representatives
+            </h1>
+
+            <div className="grid grid-cols-2 lg:grid-cols-3 mx-1 lg:mx-28 mt-24 lg:mt-40 gap-10  relative">
+              <ProfileCard designation={"Vice President"} />
+              <ProfileCard designation={"Vice President"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+            </div>
+
+            <div className="w-full flex mt-7 lg:mt-14 justify-center items-center">
+              <button className="flex flex-col items-center justify-center ">
+                <h1 className={" text-[#A4161A]  font-semibold  text-xl"}>
+                  See More
+                </h1>
+                <BiChevronDown className="text-[#A4161A] text-2xl" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* technical representatives */}
+      <div className="w-screen h-[1170px] lg:h-[2080px] bg-[#FFFFFF] -z-10">
+        <div className=" relative">
+          <div className="bg-[#A4161A] absolute top-0 right-0 left-0 h-[50vh] w-full   ">
+            <h1 className="text-white font-semibold text-center text-xl mt-20 lg:mt-25 lg:text-6xl ">
+              Technical Representatives
+            </h1>
+
+            <div className="grid grid-cols-2 lg:grid-cols-3 mx-1 lg:mx-28 mt-24 lg:mt-40 gap-10  relative">
+              <ProfileCard designation={"Vice President"} />
+              <ProfileCard designation={"Vice President"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+            </div>
+
+            <div className="w-full flex mt-7 lg:mt-14 justify-center items-center">
+              <button className="flex flex-col items-center justify-center ">
+                <h1 className={" text-[#A4161A]  font-semibold  text-xl"}>
+                  See More
+                </h1>
+                <BiChevronDown className="text-[#A4161A] text-2xl" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* g&s representatives */}
+      <div className="w-screen h-[1170px] lg:h-[2080px] bg-[#FFFFFF] -z-10">
+        <div className=" relative">
+          <div className="bg-[#A4161A] absolute top-0 right-0 left-0 h-[50vh] w-full   ">
+            <h1 className="text-white font-semibold text-center text-xl mt-20 lg:mt-25 lg:text-6xl ">
+              Games & Sports Representatives
+            </h1>
+
+            <div className="grid grid-cols-2 lg:grid-cols-3 mx-1 lg:mx-28 mt-24 lg:mt-40 gap-10  relative">
+              <ProfileCard designation={"Vice President"} />
+              <ProfileCard designation={"Vice President"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+              <ProfileCard designation={"Secretary"} />
+            </div>
+
+            <div className="w-full flex mt-7 lg:mt-14 justify-center items-center">
+              <button className="flex flex-col items-center justify-center ">
+                <h1 className={" text-[#A4161A]  font-semibold  text-xl"}>
+                  See More
+                </h1>
+                <BiChevronDown className="text-[#A4161A] text-2xl" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* sac address */}
+      <Fade direction="up" triggerOnce="true" delay="50">
+        <div className=" lg:flex bg-[#A4161A]">
+          <div>
+            <Image
+              src={dean}
+              alt="FAQ"
+              className="w-[100vw]  clip-your-needful-style-down md:w-[50vw] md:clip-your-needful-style h-[100%] "
+            />
+          </div>
+          <div className="flex flex-col lg:mt-48 justify-center lg:w-[50%] text-white p-[1rem] pb-4  pl-2.5 pr-12 h-[100%]">
+            <h1 className="font-bold text-xl lg:text-6xl mb-8">SAC Office Address</h1>
+            <h1 className=" text-md lg:text-3xl mb-1 lg:mb-3">Student Activity Center</h1>
+            <h1 className=" text-md lg:text-3xl mb-1 lg:mb-3">National Institute of Technology Rourkela</h1>
+            <h1 className=" text-md lg:text-3xl mb-1 lg:mb-3">Rourkela , Odisha - 769008</h1>
+            <div className="bg-[#A4161A] rounded-md w-2/3  lg:mt-3   flex items-center">
+              <IoCall className="text-[#FFFFFF] flex justify-center  mr-2 lg:mr-5 text-md lg:text-3xl" />
+              <a
+                href="tel:+4733378901"
+                className="text-[#FFFFFF] text-md lg:text-3xl"
+              >
+                +4733378901
+              </a>
+            </div>
+            <div className="bg-[#A4161A] rounded-md w-2/3  lg:my-5   flex items-center ">
+              <BsFillEnvelopeFill className="text-[#FFFFFF] flex justify-center mr-2 lg:mr-5 text-md lg:text-3xl" />
+              <a
+                href="mailto:sdbjwbdhij@gmail.com"
+                className="text-[#FFFFFF]  text-md lg:text-2xl"
+              >
+                sdbjwbij@gmail.com
+              </a>
+            </div>
+          </div>
+        </div>
+      </Fade>
     </div>
   );
 }
--- sac-website/components/ProfileCard/ProfileCard.jsx
+++ sac-website/components/ProfileCard/ProfileCard.jsx
@@ -5,41 +5,45 @@ import { BiChevronDown } from "react-icons/bi";
 
 function ProfileCard({designation}) {
   return (
-          <div className="  w-[46vw] h-[18vh] lg:h-[20vh] md:w-[28vw]  bg-[#FFFCEB] rounded-md flex flex-col justify-center ">
-            <h1
-              className={
-               
-                " text-[#A4161A] mt-[-6vh] lg:mt-[-3vh] font-semibold mx-auto text-md lg:text-3xl"
-              }
-            >
-              {designation}
-            </h1>
-    <div className="absolute h-[50vh] w-[42vw] lg:h-[53vh] lg:w-[22vw] rounded-md mt-[99%]  lg:mt-[32%] bg-[#A4161A] ml-[2%] lg:ml-[2.5%]">
-              <img
-                src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
-                alt="..."
-                class="shadow rounded-full w-[60%] mx-auto mt-9 h-auto align-middle border-none mb-3"
-              />
-              <span className=" ml-[20%] lg:ml-[33%]  text-white font-semibold  text-md md:text-2xl">
-                RRocky Dj Don 
-              </span>
-              <div className="lg:h-[5vh] lg:w-[20vw] w-[95%] bg-[#FFFFFF] mx-auto rounded-md mt-5 mb-5 flex items-center">
-                <IoCall className="text-[#A4161A]  mx-1 lg:mx-8 text-md lg:text-3xl" />
-                <a href="tel:+4733378901" className="text-[#A4161A] ml-3 lg:ml-0 text-md lg:text-2xl">
-                  +4733378901
-                </a>
-              </div>
-              <div className="lg:h-[5vh] lg:w-[20vw] w-[95%] bg-[#FFFFFF] mx-auto rounded-md mt-5 mb-5  flex items-center">
-                <BsFillEnvelopeFill className="text-[#A4161A] mx-1 lg:mx-8 text-md lg:text-3xl" />
-                <a
-                  href="mailto:sdbjwbdhij@gmail.com"
-                  className="text-[#A4161A]  text-sm lg:text-2xl"
-                >
-                  sdbjwbij@gmail.com
-                </a>
-              </div>
-            </div>
-          </div>
+    <div className=" w-[165px] lg:w-[570px] h-[260px] lg:h-[800px] rounded-md overflow-hidden flex flex-col ">
+    <div className="bg-[#fffceb] h-[40%] rounded-md flex items-center justify-center">
+      <h1
+        className={
+          " text-[#A4161A] font-bold  text-md md:text-4xl"
+        }
+      >
+        {designation}
+      </h1>
+    </div>
+    <div className="rounded-md h-[120%] bg-[#A4161A] mx-4 lg:mx-14 -mt-6 lg:-mt-12">
+      <img
+        src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
+        alt="..."
+        class="shadow rounded-full w-16 lg:w-48 mx-auto mt-4 lg:mt-9 align-middle border-none mb-3"
+      />
+      <p className="flex justify-center  lg:my-8 text-white font-semibold  text-md md:text-2xl">
+        RRocky Dj Don
+      </p>
+      <div className="bg-[#FFFFFF] rounded-md my-3 mx-2 lg:my-7 lg:p-3 lg:mx-5 flex items-center">
+        <IoCall className="text-[#A4161A]  mx-1 md:mx-8 text-xs md:text-3xl" />
+        <a
+          href="tel:+4733378901"
+          className="text-[#A4161A] text-xs md:text-3xl"
+        >
+          +4733378901
+        </a>
+      </div>
+      <div className="bg-[#FFFFFF] rounded-md  mx-2 lg:my-5 lg:p-3 lg:mx-5 flex items-center justify-evenly">
+        <BsFillEnvelopeFill className="text-[#A4161A] flex justify-center text-md md:text-3xl" />
+        <a
+          href="mailto:sdbjwbdhij@gmail.com"
+          className="text-[#A4161A]  text-xs md:text-2xl"
+        >
+          sdbjwbij@gmail.com
+        </a>
+      </div>
+    </div>
+  </div>
   )
 
 }
