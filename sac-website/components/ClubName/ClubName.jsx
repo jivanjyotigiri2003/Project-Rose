69d0a40fc5a1e59b5bc8f9209f9a885ce6e38310
Aankirz <sahuankit453@gmail.com>
2023-03-14 10:58:48 +0530

    ok..

--- sac-website/components/ClubName/ClubName.jsx
+++ sac-website/components/ClubName/ClubName.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
 export default function ClubName() {
+ 
+     const clubData=[
+          {
+
+          },
+     ]
   
-  
+
 return (
    <div>
         <h1>Club Name</h1>
-        
+        <div>
+            
+
+        </div>
    </div>
   )
 }
