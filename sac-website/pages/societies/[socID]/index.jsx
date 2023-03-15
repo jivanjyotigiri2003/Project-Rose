b3c9c544dfdc9c7a24040876fa5a74da2f8cea57
Aashirbad Sabat <aum.aashirbad@gmail.com>
2023-03-15 22:24:33 +0530

    Minor Channges Made

--- sac-website/pages/societies/[socID]/index.jsx
+++ sac-website/pages/societies/[socID]/index.jsx
@@ -1,89 +1,82 @@
-import React from 'react'
-import Link from 'next/link'
-import {useRouter}  from 'next/router';
-import ClubName from '@/components/ClubName/ClubName';
-import DeanSpeaks from '@/components/DeanSpeaks/DeanSpeaks';
-import SocietyGallery from '@/components/SocietyGallery/SocietyGallery';
-import SocietyRepresentative from '@/components/SocietyRepresentative/SocietyRepresentative';
-import {socData} from '../index.jsx'
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import ClubName from "@/components/ClubName/ClubName";
+import DeanSpeaks from "@/components/DeanSpeaks/DeanSpeaks";
+import SocietyGallery from "@/components/SocietyGallery/SocietyGallery";
+import SocietyRepresentative from "@/components/SocietyRepresentative/SocietyRepresentative";
+import { socData } from "../index.jsx";
 
-import clubData from '../../../data/data.js';
-
+import clubData from "../../../data/data.js";
 
 export default function soc() {
   const router = useRouter();
-  const {socID} = router.query;
-       console.log(socID);
-let socName="Technical"
-  if(socID==2)
-  socName="Film & Music"
-    else if(socID==3)   
-    socName="Literay & Culture"
-    else if(socID==4)
-    socName="Games & Sports"
- 
+  const { socID } = router.query;
+  console.log(socID);
+  let socName = "Technical";
+  if (socID == 2) socName = "Film & Music";
+  else if (socID == 3) socName = "Literay & Culture";
+  else if (socID == 4) socName = "Games & Sports";
 
-  
-    return (
-        <>
-        <div>
-           
-          <div className="h-[100vh] w-[100vw] relative justify-center align-middle flex flex-col">
-            
-            
-            <div className=" text-center tracking-wide text-6xl font-bold  text-white z-50 saturate-100 bg-cover top-0 left-0 ">
-             {socName} Society
-              <p className="  pt-10 text-xl font-semibold tracking-wide ">
-                THE TECHNICAL SOCIETY OF STUDENT ACTIVITY CENTRE, NIT
-                <br />
-                ROURKELA IS PROUD TO PRESENT ITS WORKING AND
-                <br />
-                ENTOURAGE TO ALL THE TECHNOCRATS AND INNOVATORS OF
-                <br />
-                MODERN ERA.
-              </p>
+  return (
+    <>
+      <div className="bg-[url('../public/heroimg.png')] bg-no-repeat bg-cover bg-center w-full min-h-screen overflow-x-hidden">
+        <div className="  bg-gradient-to-b from-[#850101cc] to-[#8501011a] bg-no-repeat w-full bg-cover bg-center min-h-screen">
+          <div className="bg-[#00000073] bg-no-repeat w-full bg-cover bg-center min-h-screen">
+            <div className="flex justify-center items-center min-h-screen">
+              <div className=" text-center tracking-wide text-6xl font-bold  text-white z-50 saturate-100 bg-cover top-0 left-0 ">
+                {socName} Society
+                <p className="  pt-10 text-xl font-semibold tracking-wide ">
+                  THE TECHNICAL SOCIETY OF STUDENT ACTIVITY CENTRE, NIT
+                  <br />
+                  ROURKELA IS PROUD TO PRESENT ITS WORKING AND
+                  <br />
+                  ENTOURAGE TO ALL THE TECHNOCRATS AND INNOVATORS OF
+                  <br />
+                  MODERN ERA.
+                </p>
+              </div>
             </div>
-            <div className="absolute z-10 saturate-100 bg-cover top-0 left-0 h-[100%] w-[100%] bg-[url('../public/heroimg.png')]"></div>
-            <div className="absolute z-20 opacity-30 h-[100%] top-0 left-0 w-[100%] bg-[#0B0B0B]"></div>
-            <div className="absolute z-40 opacity-30 h-[100%] top-0 left-0 w-[100%] bg-gradient-to-b from-[rgba(133,1,1,94)] to-[rgb(133,1,1)] "></div>
           </div>
         </div>
-        <DeanSpeaks data="About Technincal Society"/>
-        <SocietyRepresentative/>
-        
-        <ClubName socName={socID}/>
-        
-        <SocietyGallery/>
-      </>
-  )
+      </div>
+      <DeanSpeaks data="About Technincal Society" />
+      <SocietyRepresentative />
+
+      <ClubName socName={socID} />
+
+      <SocietyGallery />
+    </>
+  );
 }
 
-export async function getStaticPaths(){
-     const socName = socData;
+export async function getStaticPaths() {
+  const socName = socData;
 
-     const allPaths=socName.map((soc)=>{
-          return{
-               params:{
-                socID:soc.name.toString()
-              }
-          }
-     }
-      )
+  const allPaths = socName.map((soc) => {
+    return {
+      params: {
+        socID: soc.name.toString(),
+      },
+    };
+  });
 
-      return {
-        paths: allPaths,
-        fallback: false,
-      };
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
 }
 
-export function getStaticProps({params}){
-      const clubName= clubData;
-       console.log(clubName); 
-      const data=clubName.filter((club)=>{return club.clubSociety==params.socID});
+export function getStaticProps({ params }) {
+  const clubName = clubData;
+  console.log(clubName);
+  const data = clubName.filter((club) => {
+    return club.clubSociety == params.socID;
+  });
 
-      return{
-            props:{
-                data
-            }
-      }
-  }
+  return {
+    props: {
+      data,
+    },
+  };
+}
--- sac-website/pages/societies/index.jsx
+++ sac-website/pages/societies/index.jsx
@@ -1,53 +1,55 @@
-import React from 'react'
-import Link from 'next/link'
-import {useRouter}  from 'next/router';
-import clubData from '../../data/data.js';
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import clubData from "../../data/data.js";
 
-
-export const socData=[
+export const socData = [
   {
-      name:"Technical Society",
-      image:"https://ik.imagekit.io/tvfucug8t/315944295_683489136672980_8046188745263207622_n_1.png?updatedAt=1678715474291",
+    name: "Technical Society",
+    image:
+      "https://ik.imagekit.io/tvfucug8t/315944295_683489136672980_8046188745263207622_n_1.png?updatedAt=1678715474291",
   },
   {
-      name:"Film and Music Society",
-      image:"https://ik.imagekit.io/tvfucug8t/fms_3.png?updatedAt=1678715231100"
+    name: "Film and Music Society",
+    image: "https://ik.imagekit.io/tvfucug8t/fms_3.png?updatedAt=1678715231100",
   },
   {
-      name:"Literary and Culture Society",
-      image:"https://ik.imagekit.io/tvfucug8t/299931819_5474832855928681_3837718550837062886_n_2.png?updatedAt=1678715222688",
+    name: "Literary and Culture Society",
+    image:
+      "https://ik.imagekit.io/tvfucug8t/299931819_5474832855928681_3837718550837062886_n_2.png?updatedAt=1678715222688",
   },
   {
-      name:"Games and Sports Society",
-      image:"https://ik.imagekit.io/tvfucug8t/313401932_672173041137923_522549174449123500_n_1.png?updatedAt=1678715228859"
-  }
-]
-
+    name: "Games and Sports Society",
+    image:
+      "https://ik.imagekit.io/tvfucug8t/313401932_672173041137923_522549174449123500_n_1.png?updatedAt=1678715228859",
+  },
+];
 
 export default function societies() {
   const router = useRouter();
-  const {socID} = router.query;
-   
+  const { socID } = router.query;
+
   console.log(clubData);
-  
-  
-    return (
+
+  return (
     <div className="h-screen w-screen flex ">
-        <div className="absolute h-screen w-screen z-999 bg-black-500/[.8]"></div>
-      {socData.map((soc,index)=>(
-       
-            <div className="w-[25%]  h:text-[5vw] h:shadow-lg" key={index}>
-             <Link href={`/societies/${soc.name}`}><h1 className="absolute h-[100%] w-[25%] p-[1rem] top-[40%] text-[3vw] text-vertical z-100 text-white text-center h:text-underline " >{soc.name} </h1> 
-            </Link>
-            <img src={soc.image} className="h-[100%] w-[100%] object-cover -z-10"/>
-            </div>
-       
+      <div className="absolute h-screen w-screen z-999 bg-black/70"></div>
+      {socData.map((soc, index) => (
+        <div
+          className="w-[25%]  h:text-[5vw] h:shadow-lg transition duration-700 ease-in-out hover:scale-110"
+          key={index}
+        >
+          <Link href={`/societies/${soc.name}`}>
+            <h1 className="absolute h-[100%] w-[25%] p-[1rem] top-[40%] text-[3vw] text-vertical z-100 text-white text-center h:text-underline">
+              {soc.name}
+            </h1>
+          </Link>
+          <img
+            src={soc.image}
+            className="h-[100%] w-[100%] object-cover -z-10 "
+          />
+        </div>
       ))}
-        
-           
-       
-
     </div>
-  )
+  );
 }
-
