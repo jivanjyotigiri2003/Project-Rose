5d90058902000f4af6e84e167f3c8237e7e8e0d4
Sumant.xD <st72365@gmail.com>
2023-03-18 00:03:41 +0530

    info added

--- sac-website/components/SocietyRepresentative/SocietyRepresentative.jsx
+++ sac-website/components/SocietyRepresentative/SocietyRepresentative.jsx
@@ -2,99 +2,112 @@ import React from "react";
 import SocietyCarousel from "./SocietyCarouselMobile";
 import { Fade } from "react-awesome-reveal";
 
-export default function SocietyRepresentative({data='SAC Officials'}) {
+export default function SocietyRepresentative({ data = "SAC Officials" }) {
   const representative1 = [
-
     {
       officialsImage:
-        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1679067314/SacOfficials/_SMX0028_cenbfl.jpg",
       name: "Prof. Poonam Singh",
       designation: "President",
     },
-    {
-      officialsImage:
-        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-      name: "Prof. Poonam Singh",
-      designation: "President",
-    },
-    {
-      officialsImage:
-        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-      name: "Prof. Poonam Singh",
-      designation: "President",
-    },
-   
-    
-    
+    // {
+    //   officialsImage:
+    //     "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+    //   name: "Prof. Poonam Singh",
+    //   designation: "President",
+    // },
+    // {
+    //   officialsImage:
+    //     "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+    //   name: "Prof. Poonam Singh",
+    //   designation: "President",
+    // },
   ];
-const representative2 =[
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-]
+  const representative2 = [
+    {
+      officialsImage:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1679067307/SacOfficials/_SMX0005_zusai0.jpg",
+      name: "Kaustav Chaudhury",
+      designation: "Vice-President, Technical Society",
+    },
+    {
+      officialsImage:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1679067300/SacOfficials/_SMX0057_cdkl0w.jpg",
+      name: "Prasun Chongder",
+      designation: "Vice-President, Technical Society",
+    },
+    {
+      officialsImage:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1679067300/SacOfficials/_SMX0025_tkytpq.jpg",
+      name: "Winny Routray",
+      designation: "Vice-President, Literary and Cultural Society",
+    },
+    {
+      officialsImage:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1679067300/SacOfficials/_SMX0024_faohbx.jpg",
+      name: "Sivaraman J.",
+      designation: "Vice-President, Literary and Cultural Society",
+    },
+    {
+      officialsImage:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1679067300/SacOfficials/_SMX0031_d5jege.jpg",
+      name: "Ramesh Kumar Mohapatra",
+      designation: "Vice-President, Games and Sports Society",
+    },
+    {
+      officialsImage:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1679067309/SacOfficials/_SMX0058_zr1azz.jpg",
+      name: "Mayank Yadav",
+      designation: "Vice-President, Games and Sports Society",
+    },
 
-
+    {
+      officialsImage:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1679067308/SacOfficials/_SMX0013_aoss2e.jpg",
+      name: "Balaji P.S.",
+      designation: "Vice-President, Film and Music Society",
+    },
+    {
+      officialsImage:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1679067310/SacOfficials/_SMX0019_enah3a.jpg",
+      name: "Binod Bihari Sahu",
+      designation: "Vice-President, Film and Music Society",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+  ];
 
   return (
-
     <div className="w-screen ">
-
       <Fade direction="up" triggerOnce="true" delay="50">
-        <h1 className="w-[100%] text-3xl font-[600] p-4 text-center ">{data}</h1>
+        <h1 className="w-[100%] text-3xl font-[600] p-4 text-center ">
+          {data}
+        </h1>
 
         {/* For Desktop */}
-       
+
         <div className="w-[100%]  flex justify-evenly items-center p-[2rem] flex-wrap">
           {representative1.map((item, index) => {
             return (
               <div
                 key={index}
-                className="h-[20rem] m-[2rem] flex flex-col justify-center items-center "
+                className="h-[20rem] m-[0rem] flex flex-col justify-center items-center "
               >
                 <div className="w-[15rem] h-[15rem]  rounded-[100%]  m-[1rem] drop-shadow-xl shadow-2xl">
                   <img
@@ -110,9 +123,9 @@ const representative2 =[
             );
           })}
         </div>
-       
+
         <div className="w-[100%]  flex justify-between items-center p-[2rem] xl:p-[.1rem] 2xl:p-[4rem] flex-wrap">
-        {representative2.map((item, index) => {
+          {representative2.map((item, index) => {
             return (
               <div
                 key={index}
@@ -131,11 +144,7 @@ const representative2 =[
               </div>
             );
           })}
-        
         </div>
-
-      
-      
       </Fade>
     </div>
   );
