58e56ce8b0d72bff8c24cc5ae2822e76b5ac4f75
ayushkumar <ayushverma8900@gmail.com>
2023-03-17 23:14:56 +0530

    society page contacts

--- sac-website/components/SocietyRepresentative/SocietyRepresentative.jsx
+++ sac-website/components/SocietyRepresentative/SocietyRepresentative.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import SocietyCarousel from "./SocietyCarouselMobile";
 import { Fade } from "react-awesome-reveal";
+import ContactCard from "../ContactPage/ContactCard";
 
-export default function SocietyRepresentative({data='SAC Officials'}) {
+export default function SocietyRepresentative({ data = "SAC Officials" }) {
   const representative1 = [
-
     {
       officialsImage:
         "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
@@ -23,119 +23,120 @@ export default function SocietyRepresentative({data='SAC Officials'}) {
       name: "Prof. Poonam Singh",
       designation: "President",
     },
-   
-    
-    
   ];
-const representative2 =[
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-  {
-    officialsImage:
-      "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
-    name: "Prof. Poonam Singh",
-    designation: "President",
-  },
-]
-
-
+  const representative2 = [
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+    {
+      officialsImage:
+        "https://ik.imagekit.io/gourab18/ps.jpg?updatedAt=1678788056792",
+      name: "Prof. Poonam Singh",
+      designation: "President",
+    },
+  ];
 
   return (
-
     <div className="w-screen ">
-
       <Fade direction="up" triggerOnce="true" delay="50">
-        <h1 className="w-[100%] text-3xl font-[600] p-4 text-center ">{data}</h1>
+        <h1 className="w-[100%] text-3xl font-[600] p-4 text-center ">
+          {data}
+        </h1>
 
         {/* For Desktop */}
-       
-        <div className="w-[100%]  flex justify-evenly items-center p-[2rem] flex-wrap">
+
+        <div className="grid grid-cols-[28vw_28vw_28vw] place-content-center gap-x-[5vw] pt-[2rem] gap-y-[10vh] pb-[2rem] max-[600px]:grid-cols-[80vw] ">
           {representative1.map((item, index) => {
             return (
-              <div
-                key={index}
-                className="h-[20rem] m-[2rem] flex flex-col justify-center items-center "
-              >
-                <div className="w-[15rem] h-[15rem]  rounded-[100%]  m-[1rem] drop-shadow-xl shadow-2xl">
-                  <img
-                    src={item.officialsImage}
-                    alt="officialsImage"
-                    className="w-[100%] h-[100%] rounded-[100%] object-cover"
+              <div key={index} className=" ">
+                <div
+                  style={{
+                    boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <ContactCard
+                    name="Prof. Poonam Singh"
+                    designation="President SAC"
+                    email="sdbjwbdhij@gmail.com"
+                    color1="white"
+                    color2="black"
+                    color3="red"
+                    // stroke={stroke}
                   />
                 </div>
-
-                <h2 className="font-bold text-lg">{item.name}</h2>
-                <h5 className="text-lg">{item.designation}</h5>
               </div>
             );
           })}
         </div>
-       
-        <div className="w-[100%]  flex justify-between items-center p-[2rem] xl:p-[.1rem] 2xl:p-[4rem] flex-wrap">
-        {representative2.map((item, index) => {
+
+        <div className="grid grid-cols-[22vw_22vw_22vw_22vw] place-content-center gap-x-[3vw] pt-[2rem] gap-y-[10vh] pb-[2rem] society-card">
+          {representative2.map((item, index) => {
             return (
-              <div
-                key={index}
-                className="h-[20rem] m-[2rem] flex flex-col justify-center items-center "
-              >
-                <div className="w-[15rem] h-[15rem]  rounded-[100%]  m-[1rem] drop-shadow-xl shadow-2xl">
-                  <img
-                    src={item.officialsImage}
-                    alt="officialsImage"
-                    className="w-[100%] h-[100%] rounded-[100%] object-cover"
+              <div key={index} className="">
+                <div
+                  style={{
+                    boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <ContactCard
+                    name="Prof. Poonam Singh"
+                    designation="President SAC"
+                    email="sdbjwbdhij@gmail.com"
+                    color1="white"
+                    color2="black"
+                    color3="red"
+                    // stroke={stroke}
                   />
                 </div>
-
-                <h2 className="font-bold text-lg">{item.name}</h2>
-                <h5 className="text-lg">{item.designation}</h5>
               </div>
             );
           })}
-        
         </div>
-
-      
-      
       </Fade>
     </div>
   );
